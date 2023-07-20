22b0a0becd87f8619cc6344e0a5c8794b3447515
yaruneki <92526950+yaruneki@users.noreply.github.com>
2023-07-20 06:28:25 +0200

    modificate funzioni per sfondo header

--- WebContent/js/metodoPagamento.js
+++ WebContent/js/metodoPagamento.js
@@ -1,16 +1,19 @@
-function openPopUp(){
-    let box = document.getElementsByClassName("modifica-pagamento")[0];
-    box.classList.add("activeModifica");
+function openPopUp() {
+	let box = document.getElementsByClassName("modifica-pagamento")[0];
+	box.classList.add("activeModifica");
 
-    let overlay = document.getElementById("overlay")
-    overlay.classList.add("activeOverlay");
+	let overlay = document.getElementById("overlay")
+	overlay.classList.add("activeOverlay");
+
+	document.getElementById("defNav").style.backgroundColor = "transparent";
 }
 
 function closePopUp() {
-    let box = document.getElementsByClassName("modifica-pagamento")[0];
-    box.classList.remove("activeModifica");
+	let box = document.getElementsByClassName("modifica-pagamento")[0];
+	box.classList.remove("activeModifica");
 
-    let overlay = document.getElementById("overlay");
-    overlay.classList.remove("activeOverlay");
+	let overlay = document.getElementById("overlay");
+	overlay.classList.remove("activeOverlay");
 
+	document.getElementById("defNav").style.backgroundColor = "white";
 }
--- WebContent/js/modificaIndirizzo.js
+++ WebContent/js/modificaIndirizzo.js
@@ -1,17 +1,20 @@
-function openPopUpIndirizzo(){
-    let box = document.getElementsByClassName("modifica-indirizzo")[0];
-    box.classList.add("activeModifica");
+function openPopUpIndirizzo() {
+	let box = document.getElementsByClassName("modifica-indirizzo")[0];
+	box.classList.add("activeModifica");
 
-    let overlay = document.getElementById("overlayIndirizzo")
-    overlay.classList.add("activeOverlay");
+	let overlay = document.getElementById("overlayIndirizzo")
+	overlay.classList.add("activeOverlay");
+
+	document.getElementById("defNav").style.backgroundColor = "transparent";
 }
 
-
 function closePopUpIndirizzo() {
-    let box = document.getElementsByClassName("modifica-indirizzo")[0];
-    box.classList.remove("activeModifica");
+	let box = document.getElementsByClassName("modifica-indirizzo")[0];
+	box.classList.remove("activeModifica");
 
-    let overlay = document.getElementById("overlayIndirizzo");
-    overlay.classList.remove("activeOverlay");
+	let overlay = document.getElementById("overlayIndirizzo");
+	overlay.classList.remove("activeOverlay");
+
+	document.getElementById("defNav").style.backgroundColor = "white";
 
 }
